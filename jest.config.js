89c75c2fc09afbb6bd39aa4e9329d52fda426370
phpb-com <phpb-com@users.noreply.github.com>
2022-08-21 07:41:57 +0900

    tests work now, but only at React 17 level. TODO: move away from enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  setupFiles: ['<rootDir>src/setupTestsEnv.js'],
   setupFilesAfterEnv: ['<rootDir>src/setupTests.js'],
   coverageReporters: ['html', 'text-summary', 'cobertura', 'lcov'],
   collectCoverageFrom: ['src/**/*.js'],
   coveragePathIgnorePatterns: ['setupTests.js'],
+  testEnvironment: 'jsdom',
 };
